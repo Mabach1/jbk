f0ee652dded9db6ff761b65ed6c81b60b62a2cf4
mabachi <matej.baliga@gmail.com>
2023-11-20 15:42:33 +0100

    basic readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# JBK compression for TGA files
+This is a repository for JBK
+JBK is simple CLI application for compressing and decompressing of TGA files
+
+### Usage
+#### Compression
+```
+$ .bin/jbk compress --max-diff 2 --input example.tga --output out.jbk --block-size 4
+```
+#### Decompression
+```
+$ .bin/jbk decompress --input out.jbk --output decompressed.tga
+```
+
+### Sources
+* UPR scrips: https://mrlvsb.github.io/upr-skripta/c/aplikovane_ulohy/tga.html?highlight=tga#tga
+  * 
--- build.sh
+++ build.sh
@@ -2,12 +2,8 @@
 
 set -x
 
-clear
-
-if gcc -O2 -Wall -Wextra -Wpedantic -Werror -fsanitize=address -o bin/main ./src/*.c; then
-	#./bin/main compress --input a --output a --max-diff a --block-size a
-	#./bin/main a
-	./bin/main decompress --input examples/compressed/test.jbk --output examples/images/test.tga
+if gcc -O2 -Wall -Wextra -Wpedantic -Werror -fsanitize=address -o bin/jbk ./src/*.c; then
+	#./bin/main decompress --input examples/compressed/test.jbk --output examples/images/test.tga
 	#./bin/main compress --input examples/images/carmack.tga --output examples/compressed/test.jbk --max-diff 50 --block-size 4
 else
 	echo "Compilation failed."
