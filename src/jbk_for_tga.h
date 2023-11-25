54dc631adede73ddb55b5f36ec5b12fb1fa03126
mabachi <matej.baliga@gmail.com>
2023-11-25 10:32:54 +0100

    updated documentation

--- src/jbk_for_tga.h
+++ src/jbk_for_tga.h
@@ -1,7 +1,7 @@
 /**
  * @file        jbk_for_tga.h
  * @author      Matej Baliga, FEI VSB-TUO, BAL0312 (matej.baliga.stv@vsb.cz)
- * @brief       JBK (jednoducha blokova komprese) for TGA files
+ * @brief       JBK (jednoducha blokova komprese) compression of TGA files
  * @date        2024-1-2
  *
  * @copyright   Copyright (c) 2024
