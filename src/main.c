dbfd168441f0641706e2da4b772c8f7370735fbb
Mabach1 <matej.baliga@gmail.com>
2023-12-29 22:59:38 +0100

    debugging options

--- src/main.c
+++ src/main.c
@@ -6,6 +6,10 @@ void compress(const CompressArgs *args) {
     uint32_t num_pixel = 0;
     JBKPixel *compressed_image = jbk_compress_tga(&tga, args, &num_pixel);
 
+#ifdef DEBUG
+    fprintf(stderr, "\n+ JBK DEBUG : [TGA File [%s], width = %u, height = %u]\n\n", args->input, tga.header.width, tga.header.width);
+#endif
+
     jbk_save_file(args->output, compressed_image, &tga, args->block_size, num_pixel);
 
     tga_close_file(&tga);
@@ -16,6 +20,12 @@ void decompress(const DecompressArgs *args) {
     TGAFile tga = jbk_decompress_to_tga(&jbk);
 
     tga_save_file(args->output, &tga);
+
+#ifdef DEBUG
+    fprintf(stderr, "\n + JBK DEBUG : [JBK File [%s]; jbk header, block size = %u, number of compressed pixels = %u]\n", args->input, jbk.jbk_header.block_size, jbk.jbk_header.len);
+    fprintf(stderr, " + JBK DEBUG : [JBK File [%s]; tga header , width = %u, height = %u]\n", args->input, jbk.tga_header.width, jbk.tga_header.height);
+    fprintf(stderr, " + JBK DEBUG : [TGA File [%s], width = %u, height = %u]\n\n", args->output, tga.header.width, tga.header.width);
+#endif
 
     tga_close_file(&tga);
     jbk_close_file(&jbk);
